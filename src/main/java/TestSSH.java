418e2dd977bf4567cd6873682b27141c2435be0e
Alex Bleasdale <ableasdale@marklogic.com>
2017-12-15 22:52:31 +0000

    Refactoring sudo calls into Util class

--- src/main/java/TestSSH.java
+++ src/main/java/TestSSH.java
@@ -1,15 +1,7 @@
-import net.schmizz.sshj.SSHClient;
-import net.schmizz.sshj.transport.TransportException;
-import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
-import net.schmizz.sshj.userauth.UserAuthException;
-import net.schmizz.sshj.userauth.keyprovider.PKCS8KeyFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.security.Security;
 
 public class TestSSH {
 
@@ -18,32 +10,14 @@ public class TestSSH {
     public static void main(String[] args) {
 
         Util.JcePolicyFix();
-        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-        SSHClient client = new SSHClient();
-        client.addHostKeyVerifier(new PromiscuousVerifier());
 
-        try {
-            client.connect(Util.getConfiguration().getString("host"));
-        } catch (IOException e) {
-            LOG.error("IOException: ", e);
-        }
-
-        PKCS8KeyFile keyFile = new PKCS8KeyFile();
-        keyFile.init(new File(Util.getConfiguration().getString("pemfile")));
-        try {
-            client.authPublickey(Util.getConfiguration().getString("sshuser"), keyFile);
-        } catch (UserAuthException e) {
-            LOG.error("UserAuthException: ", e);
-        } catch (TransportException e) {
-            LOG.error("TransportException: ", e);
-        }
-
-
-        LOG.info(Util.execCmd(client, "whoami"));
-       // LOG.info(Util.execCmd(client, "ls -lart"));
-       // LOG.info(Util.execCmd(client, "ps aux"));
-       // LOG.info(Util.execCmd(client, "/usr/sbin/service MarkLogic status"));
-       // LOG.info(Util.execCmd(client, "/usr/local/sbin/mladmin"));
-        LOG.info(Util.execCmd(client, "sudo /usr/sbin/service MarkLogic pstack"));
+        LOG.info(Util.execCmd(Util.getSSHClient(), "whoami"));
+        LOG.info(Util.execCmd(Util.getSSHClient(), "ls -lart"));
+        LOG.info(Util.execCmd(Util.getSSHClient(), "ps aux"));
+        LOG.info(Util.execCmd(Util.getSSHClient(), "/usr/sbin/service MarkLogic status"));
+        LOG.info(Util.execCmd(Util.getSSHClient(), "/usr/local/sbin/mladmin"));
+        // /usr/sbin/service MarkLogic pstack
+        LOG.info(Util.execSudoCmd(Util.getSSHClient(), "/usr/sbin/service MarkLogic pstack"));
+        LOG.info(Util.execSudoCmd(Util.getSSHClient(), "/usr/sbin/service MarkLogic pmap"));
     }
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -3,6 +3,11 @@ import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.transport.TransportException;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.userauth.UserAuthException;
+import net.schmizz.sshj.userauth.keyprovider.PKCS8KeyFile;
+import net.sf.expectit.Expect;
+import net.sf.expectit.ExpectBuilder;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
@@ -14,12 +19,17 @@ import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.security.Security;
 import java.util.concurrent.TimeUnit;
+
+import static net.sf.expectit.matcher.Matchers.contains;
 
 public class Util {
 
     private static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static SSHClient CLIENT = null;
     private static Configuration CONFIG = null;
+    private static String HOSTNAME = Util.getConfiguration().getString("host").substring(0, Util.getConfiguration().getString("host").indexOf("."));
 
     protected static void JcePolicyFix() {
         try {
@@ -40,6 +50,30 @@ public class Util {
         }
     }
 
+    protected static SSHClient getSSHClient() {
+        if (CLIENT != null) {
+            return CLIENT;
+        } else {
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            CLIENT = new SSHClient();
+            CLIENT.addHostKeyVerifier(new PromiscuousVerifier());
+            try {
+                CLIENT.connect(Util.getConfiguration().getString("host"));
+            } catch (IOException e) {
+                LOG.error("IOException: ", e);
+            }
+            PKCS8KeyFile keyFile = new PKCS8KeyFile();
+            keyFile.init(new File(Util.getConfiguration().getString("pemfile")));
+            try {
+                CLIENT.authPublickey(Util.getConfiguration().getString("sshuser"), keyFile);
+            } catch (UserAuthException e) {
+                LOG.error("UserAuthException: ", e);
+            } catch (TransportException e) {
+                LOG.error("TransportException: ", e);
+            }
+            return CLIENT;
+        }
+    }
 
     protected static String execCmd(SSHClient client, String cmd) {
         String response = null;
@@ -60,12 +94,39 @@ public class Util {
         return response;
     }
 
+    protected static String execSudoCmd(SSHClient client, String cmd) {
+        String response = null;
+        try {
+            Session session;
+            Session.Shell shell;
+            session = client.startSession();
+            session.allocateDefaultPTY();
+            shell = session.startShell();
+            Expect expect = new ExpectBuilder()
+                    .withOutput(shell.getOutputStream())
+                    .withInputs(shell.getInputStream(), shell.getErrorStream())
+                    .build();
+            expect.sendLine(String.format("sudo %s", cmd));
+            expect.expect(contains(String.format("[sudo] password for %s:", Util.getConfiguration().getString("sshuser"))));
+            expect.sendLine(Util.getConfiguration().getString("supasswd"));
+            response = expect.expect(contains(String.format("[%s@%s ~]$", Util.getConfiguration().getString("sshuser"), HOSTNAME))).getBefore();
+            expect.close();
+            session.close();
+        } catch (ConnectionException e) {
+            LOG.error("ConnectionException: ", e);
+        } catch (TransportException e) {
+            LOG.error("TransportException: ", e);
+        } catch (IOException e) {
+            LOG.error("IOException: ", e);
+        }
+        return response;
+    }
 
     protected static Configuration getConfiguration() {
         if (CONFIG != null) {
             return CONFIG;
         } else {
-            LOG.info("trying to get config!");
+            LOG.info("trying to get configuration for the first time");
             Configurations configs = new Configurations();
             try {
                 CONFIG = configs.properties(new File("config.properties"));
